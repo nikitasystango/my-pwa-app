2e129cf4669e35a4d87a3a6e4e201323df889851
mb104 <mb104@mb104s-MacBook-Pro.local>
2021-11-11 12:38:48 +0530

    Added a code to reload window in app.js container.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -150,7 +150,7 @@ const Home = (props) => {
                     : intl(staticMessage.sectionTitle)}
                 </SectionTitle>
                 <SectionSubTitle>
-                  {intl(staticMessage.sectionSubTitle)} hellloooo
+                  {intl(staticMessage.sectionSubTitle)}
                 </SectionSubTitle>
               </HeaderContent>
             )}
